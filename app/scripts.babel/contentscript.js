ca475fbd84b75bbdb0e40a84818cec1d66b3635e
Yuki Hirai <yukihirai0505@gmail.com>
2018-08-21 10:54:17 +0900

    Add formatFactory function

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,6 +2,48 @@
 
 $(function () {
 
+  function formatFactory(html) {
+    function parse(_html, tab = 0) {
+      let html = $.parseHTML(_html),
+        formatHtml = String();
+
+      function setTabs() {
+        let tabs = String();
+
+        for (let i = 0; i < tab; i++) {
+          tabs += '\t';
+        }
+        return tabs;
+      }
+
+      $.each(html, function (i, el) {
+        if (el.nodeName === '#text') {
+          if (($(el).text().trim()).length) {
+            formatHtml += setTabs() + $(el).text().trim() + '\n';
+          }
+        } else {
+          let innerHTML = $(el).html().trim();
+          $(el).html(innerHTML.replace('\n', '').replace(/ +(?= )/g, ''));
+
+
+          if ($(el).children().length) {
+            $(el).html('\n' + parse(innerHTML, (tab + 1)) + setTabs());
+            let outerHTML = $(el).prop('outerHTML').trim();
+            formatHtml += setTabs() + outerHTML + '\n';
+
+          } else {
+            let outerHTML = $(el).prop('outerHTML').trim();
+            formatHtml += setTabs() + outerHTML + '\n';
+          }
+        }
+      });
+
+      return formatHtml;
+    }
+
+    return parse(html.replace(/(\r\n|\n|\r)/gm, ' ').replace(/ +(?= )/g, ''));
+  }
+
   let switchFlg = false,
     alisHtmlEditorId = 'alis-html-editor',
     htmlConvertBtnDefaultText = chrome.i18n.getMessage('htmlConvertBtnDefaultMessage'),
@@ -20,7 +62,7 @@ $(function () {
       let $htmlTextarea = $('<textarea>').attr({
         id: alisHtmlEditorId,
         rows: 500
-      }).val($areaBody.html());
+      }).val(formatFactory($areaBody.html()));
       $areaBody.after($htmlTextarea).css({display: 'none'});
       $htmlConvertBtn.text(chrome.i18n.getMessage('htmlConvertBtnReturnMessage'));
     }
