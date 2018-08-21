8125c8a00c141458bb71edbbd5f23c5fd3721393
Yuki Hirai <yukihirai0505@gmail.com>
2018-08-21 11:30:54 +0900

    Add utils.js

---
+++ app/scripts.babel/utils.js
@@ -0,0 +1,41 @@
+function formatFactory(html) {
+  function parse(_html, tab = 0) {
+    let html = $.parseHTML(_html),
+      formatHtml = String();
+
+    function setTabs() {
+      let tabs = String();
+
+      for (let i = 0; i < tab; i++) {
+        tabs += '\t';
+      }
+      return tabs;
+    }
+
+    $.each(html, function (i, el) {
+      if (el.nodeName === '#text') {
+        if (($(el).text().trim()).length) {
+          formatHtml += setTabs() + $(el).text().trim() + '\n';
+        }
+      } else {
+        let innerHTML = $(el).html().trim();
+        $(el).html(innerHTML.replace('\n', '').replace(/ +(?= )/g, ''));
+
+
+        if ($(el).children().length) {
+          $(el).html('\n' + parse(innerHTML, (tab + 1)) + setTabs());
+          let outerHTML = $(el).prop('outerHTML').trim();
+          formatHtml += setTabs() + outerHTML + '\n';
+
+        } else {
+          let outerHTML = $(el).prop('outerHTML').trim();
+          formatHtml += setTabs() + outerHTML + '\n';
+        }
+      }
+    });
+
+    return formatHtml;
+  }
+
+  return parse(html.replace(/(\r\n|\n|\r)/gm, ' ').replace(/ +(?= )/g, ''));
+}
