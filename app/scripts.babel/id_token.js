bbf739ec61c4745495facdd035a6b3a6f810aaef
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-06 18:00:13 +0900

    Update id token btn handling

--- app/scripts.babel/id_token.js
+++ app/scripts.babel/id_token.js
@@ -2,14 +2,22 @@
 
 $(function () {
 
-  let $idTokenBtn = $('<button>').addClass('id-token-btn-txt').text(chrome.i18n.getMessage('idTokenBtnDefaultMessage'));
+  let $idTokenBtn = $('<button>').addClass('id-token-btn-txt').text(chrome.i18n.getMessage('idTokenBtnDefaultMessage')),
+    $idTokenInput = $('<input>').attr({'type': 'text'});
 
   $idTokenBtn.on('click', function () {
     let name = localStorage.getItem('CognitoIdentityServiceProvider.2gri5iuukve302i4ghclh6p5rg.LastAuthUser'),
       id_token = localStorage.getItem('CognitoIdentityServiceProvider.2gri5iuukve302i4ghclh6p5rg.' + name + '.idToken');
-    alert(id_token);
+    $idTokenInput.val(id_token);
+    $idTokenInput.select();
+    document.execCommand('copy');
+    alert('copied id token');
+    $idTokenInput.val('');
   });
 
-  let $idTokenBtnDiv = $('<div>').addClass('id-token-btn').append($idTokenBtn);
-  $('body').append($idTokenBtnDiv);
+  let $idTokenBtnDiv = $('<div>').addClass('id-token-btn')
+    .append($idTokenBtn);
+  $('body')
+    .append($idTokenBtnDiv)
+    .append($idTokenInput);
 });
