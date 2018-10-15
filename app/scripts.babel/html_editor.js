9bf18fa77b82e5488301af95d75d1745f1a99a24
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-15 12:28:34 +0900

    Refactoring

--- app/scripts.babel/html_editor.js
+++ app/scripts.babel/html_editor.js
@@ -1,52 +1,60 @@
-'use strict';
+'use strict'
 
-$(function () {
-
+$(function() {
   let switchFlg = false,
     alisHtmlEditorId = 'alis-html-editor',
-    htmlConvertBtnDefaultText = chrome.i18n.getMessage('htmlConvertBtnDefaultMessage'),
-    $htmlConvertBtn = $('<button>').addClass('html-convert-btn-txt').text(htmlConvertBtnDefaultText);
+    htmlConvertBtnDefaultText = chrome.i18n.getMessage(
+      'htmlConvertBtnDefaultMessage'
+    ),
+    $htmlConvertBtn = $('<button>')
+      .addClass('html-convert-btn-txt')
+      .text(htmlConvertBtnDefaultText)
 
-  $htmlConvertBtn.on('click', function () {
-    let $areaBody = $('.area-body');
+  $htmlConvertBtn.on('click', function() {
+    let $areaBody = $('.area-body')
     if (switchFlg) {
-      let $htmlTextarea = $(`#${alisHtmlEditorId}`);
-      $areaBody.empty();
-      $areaBody.append($htmlTextarea.val());
-      $htmlTextarea.remove();
-      $areaBody.css({display: ''});
+      let $htmlTextarea = $(`#${alisHtmlEditorId}`)
+      $areaBody.empty()
+      $areaBody.append($htmlTextarea.val())
+      $htmlTextarea.remove()
+      $areaBody.css({ display: '' })
 
-      $htmlConvertBtn.text(htmlConvertBtnDefaultText);
-
+      $htmlConvertBtn.text(htmlConvertBtnDefaultText)
     } else {
-      $areaBody.find('.medium-insert-buttons').remove();
-      $areaBody.find('.medium-insert-active').remove();
+      $areaBody.find('.medium-insert-buttons').remove()
+      $areaBody.find('.medium-insert-active').remove()
 
-      $areaBody.css({display: 'none'});
+      $areaBody.css({ display: 'none' })
 
-      let $htmlTextarea = $('<textarea>').attr({
-        id: alisHtmlEditorId,
-        rows: 500
-      }).val(formatFactory($areaBody.html()));
-      $areaBody.after($htmlTextarea);
+      let $htmlTextarea = $('<textarea>')
+        .attr({
+          id: alisHtmlEditorId,
+          rows: 500
+        })
+        .val(alisEx.formatFactory($areaBody.html()))
+      $areaBody.after($htmlTextarea)
 
-      $htmlConvertBtn.text(chrome.i18n.getMessage('htmlConvertBtnReturnMessage'));
+      $htmlConvertBtn.text(
+        chrome.i18n.getMessage('htmlConvertBtnReturnMessage')
+      )
     }
-    switchFlg = !switchFlg;
-  });
-  let $htmlConvertDiv = $('<div>').addClass('html-convert-btn').append($htmlConvertBtn);
-  if (isArticleEditPage(location.href)) {
-    $('body').append($htmlConvertDiv);
+    switchFlg = !switchFlg
+  })
+  let $htmlConvertDiv = $('<div>')
+    .addClass('html-convert-btn')
+    .append($htmlConvertBtn)
+  if (alisEx.isArticleEditPage(location.href)) {
+    $('body').append($htmlConvertDiv)
   }
   let href = location.href,
-    observer = new MutationObserver(function (mutations) {
-      let currentHref = location.href;
+    observer = new MutationObserver(function(mutations) {
+      let currentHref = location.href
       if (href !== currentHref) {
-        if (!isArticleEditPage(currentHref)) {
-          $htmlConvertDiv.remove();
+        if (!alisEx.isArticleEditPage(currentHref)) {
+          $htmlConvertDiv.remove()
         }
       }
-    });
+    })
 
-  observer.observe(document, {childList: true, subtree: true});
-});
+  observer.observe(document, { childList: true, subtree: true })
+})
--- app/scripts.babel/id_token.js
+++ app/scripts.babel/id_token.js
@@ -1,24 +1,24 @@
-'use strict';
+'use strict'
 
-$(function () {
+$(function() {
+  let $idTokenBtn = $('<button>')
+      .addClass('id-token-btn-txt')
+      .text(chrome.i18n.getMessage('idTokenBtnDefaultMessage')),
+    $idTokenInput = $('<input>').attr({ type: 'text' })
 
-  let $idTokenBtn = $('<button>').addClass('id-token-btn-txt').text(chrome.i18n.getMessage('idTokenBtnDefaultMessage')),
-    $idTokenInput = $('<input>').attr({'type': 'text'});
+  $idTokenBtn.on('click', function() {
+    $idTokenInput.val(alisEx.getIdToken())
+    $idTokenInput.select()
+    document.execCommand('copy')
+    alert('copied id token')
+    $idTokenInput.val('')
+  })
 
-  $idTokenBtn.on('click', function () {
-    let name = localStorage.getItem('CognitoIdentityServiceProvider.2gri5iuukve302i4ghclh6p5rg.LastAuthUser'),
-      idToken = localStorage.getItem(`CognitoIdentityServiceProvider.2gri5iuukve302i4ghclh6p5rg.${name}.idToken`);
-    $idTokenInput.val(idToken);
-    $idTokenInput.select();
-    document.execCommand('copy');
-    alert('copied id token');
-    $idTokenInput.val('');
-  });
-
-  let $idTokenBtnDiv = $('<div>').addClass('id-token-btn')
-    .append($idTokenBtn);
+  let $idTokenBtnDiv = $('<div>')
+    .addClass('id-token-btn')
+    .append($idTokenBtn)
 
   $('body')
     .append($idTokenBtnDiv)
-    .append($idTokenInput);
-});
+    .append($idTokenInput)
+})
