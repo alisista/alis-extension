8dcafd1d7d43eb34ba1f16c7b84c1587b6f0ad80
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-15 13:54:15 +0900

    Add sweetalert2

--- app/scripts.babel/tip_extension.js
+++ app/scripts.babel/tip_extension.js
@@ -6,47 +6,62 @@ $(function() {
     .text(chrome.i18n.getMessage('tipExtBtnDefaultMessage'))
 
   // 投げ銭ボタンを推した時のアクション
-  $tipExtBtn.on('click', function() {
-    //ポップアップウインドウを出して数値を聞く
-    let tipInput = window.prompt(
-      '投げ銭するALISの量を入れてください。マイナス１８桁まで行けます。現在、10ALISまでに限定しています',
-      '0.01'
-    )
-    //入力された数値判定
-    if (!tipInput) {
-      alert('10以下の半角数値を入力ください')
-      return
-    }
-    //数値化
-    let tip = Number(tipInput)
-    if (tip > 10 || tip < 0.000000000000000001) {
-      alert('10以下もしくは0.000000000000000001以上の半角数値を入力ください')
-      return
-    }
-    if (window.confirm(`${tip} ALISを送付します。OKですね？`)) {
-      let data = {
-        article_id: location.href.match(/articles\/([a-zA-Z0-9]{12})/)[1],
-        tip_value: tip
+  $tipExtBtn.on('click', async function() {
+    const { value: tip } = await swal({
+      title: '投げ銭するALISの量を入れてください',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off'
+      },
+      showCancelButton: true,
+      inputValidator: value => {
+        let tip = Number(value)
+        if (!$.isNumeric(value) || tip > 10 || tip < 0.000000000000000001) {
+          return '10以下で0.000000000000000001以上の半角数値を入力ください'
+        }
       }
-      $.ajax({
-        type: 'POST',
-        timeout: 3000,
-        url: 'https://alis.to/api/me/wallet/tip',
-        headers: {
-          Authorization: alisEx.getIdToken()
-        },
-        data: JSON.stringify(data),
-        contentType: 'application/json;charset=UTF-8',
-        dataType: 'text'
+    })
+
+    if (tip) {
+      const { value: result } = await swal({
+        text: `${tip} ALISを送付します。OKですね？`,
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33'
       })
-        .done(function(data) {
-          alert('送信に成功しました')
+      if (result) {
+        let data = {
+          article_id: location.href.match(/articles\/([a-zA-Z0-9]{12})/)[1],
+          tip_value: Number(tip)
+        }
+        $.ajax({
+          type: 'POST',
+          timeout: 3000,
+          url: 'https://alis.to/api/me/wallet/tip',
+          headers: {
+            Authorization: alisEx.getIdToken()
+          },
+          data: JSON.stringify(data),
+          contentType: 'application/json;charset=UTF-8',
+          dataType: 'text'
         })
-        .fail(function(jqXHR, textStatus, errorThrown) {
-          console.log('XMLHttpRequest : ' + XMLHttpRequest.status)
-          console.log('textStatus     : ' + textStatus)
-          console.log('errorThrown    : ' + errorThrown.message)
-        })
+          .done(function(data) {
+            swal({
+              type: 'success',
+              text: '送信に成功しました'
+            })
+          })
+          .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log('XMLHttpRequest : ' + XMLHttpRequest.status)
+            console.log('textStatus     : ' + textStatus)
+            console.log('errorThrown    : ' + errorThrown.message)
+            swal({
+              type: 'error',
+              text: '送信に失敗しました'
+            })
+          })
+      }
     }
   })
 
