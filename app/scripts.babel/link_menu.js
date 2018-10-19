8aa29e7d285217917601b5c8e35654fbcd132712
yuuki <relaxation11y@gmail.com>
2018-10-19 23:19:02 +0900

    コメント文の削除

--- app/scripts.babel/link_menu.js
+++ app/scripts.babel/link_menu.js
@@ -3,7 +3,6 @@
 $(() => {
   let $linkExtMenu = $('<a>')
     .addClass('alis-extension-link-menu')
-    //.text(chrome.i18n.getMessage('linkExtBtnDefaultMessage'))
     .html(
       '<a href="https://github.com/alisista/alis-extension/wiki">' +
         chrome.i18n.getMessage('linkExtBtnDefaultMessage') +
