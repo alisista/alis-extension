bb822cb117366fd50f1b51b9f8c575345ed09be2
yuuki <relaxation11y@gmail.com>
2018-10-20 13:38:19 +0900

    人気記事と新着記事のリンクの追加 ＋ link_menu.jsを微修正

---
+++ app/scripts.babel/alis_link_menu.js
@@ -0,0 +1,21 @@
+'use strict'
+
+$(() => {
+  let $ALISlinkExtMenu1 = $('<a>')
+    .addClass('alis-extension-alis-link-menu')
+    .html(
+      '<a href="https://alis.to/articles/popular">' +
+        chrome.i18n.getMessage('ALISlinkExtBtnDefaultMessage1') +
+        '</a>'
+    )
+  let $ALISlinkExtMenu2 = $('<a>')
+    .addClass('alis-extension-alis-link-menu')
+    .html(
+      '<a href="https://alis.to/articles/recent">' +
+        chrome.i18n.getMessage('ALISlinkExtBtnDefaultMessage2') +
+        '</a>'
+    )
+
+  $('#alis-extension-alis-link').append($ALISlinkExtMenu1)
+  $('#alis-extension-alis-link').append($ALISlinkExtMenu2)
+})
